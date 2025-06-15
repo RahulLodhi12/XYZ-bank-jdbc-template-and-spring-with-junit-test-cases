717711f629a4a16e1fde3b257367770efaa55573
Rahul Lodhi <rahullodhimts@gmail.com>
2025-06-15 16:48:33 +0530

    first commit

--- src/main/java/com/jpmc/dao/DummyDao.java
+++ src/main/java/com/jpmc/dao/DummyDao.java
@@ -3,35 +3,33 @@ package com.jpmc.dao;
 import com.jpmc.bean.Customer;
 import com.jpmc.bean.Login;
 
-import java.sql.SQLException;
-
 public class DummyDao{ //For JUnit Testing
 
-    public boolean customerLogin(Login login) {
+    public boolean customerLogin_Test(Login login) {
         if("174877832905923273".equals(login.getAccountNumber()) && "688526".equals(login.getPin())) return true;
         return false;
     }
 
 
-    public boolean createAccount(Customer customer){
+    public boolean createAccount_Test(Customer customer){
         if("174877791759875275".equals(customer.getAccountNumber()) && "rekha singh".equals(customer.getCustomerName()) && "jodhpur".equals(customer.getBranch())) return true;
         return false;
     }
 
 
-    public boolean deposit(Customer customer, Double amt) {
+    public boolean deposit_Test(Customer customer, Double amt) {
         if("174877832905923273".equals(customer.getAccountNumber()) && amt>100) return true;
         return false;
     }
 
 
-    public boolean withdraw(Customer customer, Double amt) {
+    public boolean withdraw_Test(Customer customer, Double amt) {
         if("174877832905923273".equals(customer.getAccountNumber()) && customer.getBalance()-amt>1000) return true;
         return false;
     }
 
 
-    public boolean fundTransfer(Customer customer1, Customer customer2, Double amt) {
+    public boolean fundTransfer_Test(Customer customer1, Customer customer2, Double amt) {
         if("174877832905923273".equals(customer1.getAccountNumber()) && customer1.getBalance()-amt>1000){
             if("174880400724795934".equals(customer2.getAccountNumber())) return true;
         }
@@ -39,7 +37,7 @@ public class DummyDao{ //For JUnit Testing
     }
 
 
-    public boolean isLowBalance(double balance) {
+    public boolean isLowBalance_Test(double balance) {
         if(balance<1000) return true;
         return false;
     }
--- src/test/java/com/jpmc/DaoTest.java
+++ src/test/java/com/jpmc/DaoTest.java
@@ -13,61 +13,61 @@ public class DaoTest {
 
     @Test
     void customerLogin_Success() {
-        assertTrue(dummyDao.customerLogin(new Login("174877832905923273","688526")));
+        assertTrue(dummyDao.customerLogin_Test(new Login("174877832905923273","688526")));
     }
 
     @Test
     void customerLogin_Failure() {
-        assertFalse(dummyDao.customerLogin(new Login("174877832905923299","688599")));
+        assertFalse(dummyDao.customerLogin_Test(new Login("174877832905923299","688599")));
     }
 
     @Test
     void createAccount_Success(){
-        assertTrue(dummyDao.createAccount(new Customer("174877791759875275","rekha singh","jodhpur")));
+        assertTrue(dummyDao.createAccount_Test(new Customer("174877791759875275","rekha singh","jodhpur")));
     }
 
     @Test
     void createAccount_Failure(){
-        assertFalse(dummyDao.createAccount(new Customer("174877791759875299","rekha singh","jodhpur")));
+        assertFalse(dummyDao.createAccount_Test(new Customer("174877791759875299","rekha singh","jodhpur")));
     }
 
     @Test
     void desposit_Success(){
-        assertTrue(dummyDao.deposit(new Customer("174877832905923273"),200.0));
+        assertTrue(dummyDao.deposit_Test(new Customer("174877832905923273"),200.0));
     }
 
     @Test
     void desposit_Failure(){
-        assertFalse(dummyDao.deposit(new Customer("174877832905923273"),50.0));
+        assertFalse(dummyDao.deposit_Test(new Customer("174877832905923273"),50.0));
     }
 
     @Test
     void withdraw_Success(){
-        assertTrue(dummyDao.withdraw(new Customer("174877832905923273",4500.00),1000.00));
+        assertTrue(dummyDao.withdraw_Test(new Customer("174877832905923273",4500.00),1000.00));
     }
 
     @Test
     void withdraw_Failure(){
-        assertFalse(dummyDao.withdraw(new Customer("174877832905923273",1500.00),1000.00));
+        assertFalse(dummyDao.withdraw_Test(new Customer("174877832905923273",1500.00),1000.00));
     }
 
     @Test
     void fundTransfer_Success(){
-        assertTrue(dummyDao.fundTransfer(new Customer("174877832905923273",4500.00), new Customer("174880400724795934"),2500.00));
+        assertTrue(dummyDao.fundTransfer_Test(new Customer("174877832905923273",4500.00), new Customer("174880400724795934"),2500.00));
     }
 
     @Test
     void fundTransfer_Failure(){
-        assertFalse(dummyDao.fundTransfer(new Customer("174877832905923273",4500.00), new Customer("174880400724795934"),4000.00));
+        assertFalse(dummyDao.fundTransfer_Test(new Customer("174877832905923273",4500.00), new Customer("174880400724795934"),4000.00));
     }
 
     @Test
     void isLowBalance_True(){
-        assertTrue(dummyDao.isLowBalance(500));
+        assertTrue(dummyDao.isLowBalance_Test(500));
     }
 
     @Test
     void isLowBalance_False(){
-        assertFalse(dummyDao.isLowBalance(1500));
+        assertFalse(dummyDao.isLowBalance_Test(1500));
     }
 }
